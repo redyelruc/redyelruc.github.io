04ed6f911dd09e655c628a58e90b51b16ff9122d
redyelruc <62723799+redyelruc@users.noreply.github.com>
2020-08-30 15:01:00 +0100

    First Commit to go live.

---
+++ README.md
@@ -0,0 +1,54 @@
+# Etch-a-Sketch!
+
+
+## **General Information**
+
+Etch-a-Sketch application using vanilla Javascript, HTML, and CSS.
+
+
+
+## **Author**
+
+Aidan Curley as part of The Odin Project.
+
+
+## **Contributing**
+
+**How to contribute**
+
+I'm always looking for collaborations and particularly welcome from developers looking to improve the existing code or add new features
+
+## Submitting changes
+
+Please send a pull request with a clear list of what you've done (read more about [pull requests](https://docs.github.com/en/github/collaborating-with-issues-and-pull-requests/creating-a-pull-request)). When you send a pull request, please make sure that there is only one feature added/changed per commit. Please attach a clear log message for your commits. One-line messages are fine for small changes, but bigger changes should look like this:
+
+```
+$ git commit -m "A brief summary of the commit
+> 
+> A paragraph describing what changed and its impact."
+```
+
+## **Changelog**
+
+A detailed changelog, intended for programmers
+
+## **Bugs**
+
+Currently no known bugs.
+
+
+## Thanks
+
+If you have read this far, thanks. I'm always looking for feedback and collaborations. Feel free to give me a shout.
+
+## **Copying/License**
+
+Copyright (c) **2020** Aidan Curley
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions: 
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+
---
+++ etchasketch.js
@@ -0,0 +1,121 @@
+// global variables 
+let paintable = false; //mousedown=true mouseup=false: when paintable, cells change color on mouseover.
+let eraseMode = false; // controls whether color is white or fillColor
+let defaultColor = "#009900";
+let gridSize = 40;  //number of cells in a row/column
+let fillColor; // color that cells will be painted
+let cellBorderStyle = "1px solid rgb(230,230,230)";
+
+window.addEventListener("load", startup());
+
+// set the color, delete the grid, add new grid
+function startup() {
+    let colorWell = document.querySelector("#color");
+    colorWell.value = defaultColor;
+    fillColor = defaultColor;
+    colorWell.addEventListener("change", () => {
+        fillColor = event.target.value
+    }, false);
+    colorWell.select();
+
+    let gridSizeControl = document.querySelector("#gridsize");
+    gridSizeControl.value = gridSize;
+    gridSizeControl.addEventListener("change", () => {
+        gridSize = event.target.value;
+        deleteGrid();
+        makeGrid(gridSize);
+        toggleGridlines(document.querySelector("#gridline-visibility"));
+    }, false);
+
+    deleteGrid();
+    makeGrid(gridSize);
+}
+
+// make a new grid
+function makeGrid(size) {
+    let sketchpad = document.getElementById("sketchpad");
+    sketchpad.style.setProperty('--grid-rows', size);
+    sketchpad.style.setProperty('--grid-cols', size);
+    for (c = 0; c < (size * size); c++) {
+        let cell = document.createElement("div");
+        cell.style.border = cellBorderStyle;
+        cell.style["background-color"] = "white";
+        cell.addEventListener("click", () => { paintCell(cell) });
+        cell.addEventListener("mousedown", () => { paintable = true });
+        cell.addEventListener("mouseup", () => { paintable = false });
+        cell.addEventListener("mouseover", () => { if (paintable) { paintCell(cell) } });
+        sketchpad.appendChild(cell).className = "grid-item";
+    };
+}
+
+// delete existing grid
+function deleteGrid() {
+    let sketchpad = document.getElementById("sketchpad");
+    while (sketchpad.firstChild) {
+        sketchpad.removeChild(sketchpad.lastChild);
+    }
+}
+
+
+// make gridlines visible/invisible
+function toggleGridlines(element) {
+    let cells = document.getElementsByClassName("grid-item");
+    for (let i = 0; i < cells.length; i++) {
+        if (element.checked) {
+            cells[i].style.border = cellBorderStyle;
+        } else {
+            cells[i].style.border = "none";
+        }
+    }
+}
+
+// color a cell
+function paintCell(cell) {
+    if (eraseMode) {
+        cell.style['background-color'] = "white";
+    } else {
+        cell.style['background-color'] = fillColor;
+    }
+}
+
+// ensure number entered does not exceed min and max allowed
+// if it does, change to either min or max
+function enforceMinMax(el) {
+    if (el.value != "") {
+        if (parseInt(el.value) < parseInt(el.min)) {
+            el.value = el.min;
+            el.click();
+        }
+        if (parseInt(el.value) > parseInt(el.max)) {
+            el.value = el.max;
+            el.click();
+        }
+    }
+}
+
+
+// turn erase mode on or off
+function toggleEraseMode() {
+    let button = document.getElementById("erase-toggle");
+    if (!eraseMode) {     // if not currently in erase mode, switch on
+        eraseMode = true;
+        button.style["font-weight"] = "bold";
+        button.style["background-color"] = "#ffcccc";
+        button.style.border = "1px solid red";
+    } else {     // if currently in erase mode, switch to normal
+        eraseMode = false;
+        button.style["font-weight"] = "normal";
+        button.style["background-color"] = "white";
+        button.style.border = "1px solid black";
+    }
+}
+
+function captureSketchpad() {
+    sketchpad = document.getElementById("sketchpad");
+    html2canvas(sketchpad, { onrendered:function(canvas) 
+        {
+        let imageURL = canvas.toDataURL('sketch/png');
+        window.open(imageURL);
+        }
+    });
+}
